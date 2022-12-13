144d06d7fd90e178feba8cbb6294c4ee5012feb8
unknown <hamer.tv@outlook.com>
2022-12-13 08:51:34 +0100

    first and last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Projekt';
+  imieinazw: string = "";
+  email: string="Prosze Wpisac email";
+  produkt: string = "";
+  number: number = 1;
+  wiadomosc: string = "Jesli jakis problem to napisz";
+
+  onSubmit() {
+    console.log("wszystko dziala");
+  }
 }
